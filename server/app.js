6b45aca43d4048cbd153a41eb9541dfb552c93a0
Pavel Prudnikov <p.m.prudnikov@gmail.com>
2018-07-07 00:29:25 +0300

    chore (server): add cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const db = require('./mongoose');
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
